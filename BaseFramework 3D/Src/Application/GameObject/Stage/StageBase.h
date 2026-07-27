78399e13c43a945348c35d1a89610fc88c08555a
kd13814116 <kd1381416@st.kobedenshi.ac.jp>
2026-07-27 15:37:37 +0900

    2026/07/27.Mon

---
+++ BaseFramework 3D/Src/Application/GameObject/Stage/StageBase.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class StageBase :public KdGameObject
+{
+public:
+	StageBase()						{}
+	virtual ~StageBase()	override	{}
+
+	void Init()				override;
+	void Update()			override;
+	void DrawLit()			override;
+
+private:
+
+	std::shared_ptr<KdModelData>	m_spModel = nullptr;
+
+};
